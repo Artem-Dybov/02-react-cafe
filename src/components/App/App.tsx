d382ca77642e807a807e7c0ad71e1fffa7c26a75
Artem Dybov <dybov.artem.v@gmail.com>
2025-07-21 19:43:55 +0300

    hw-2-3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -29,10 +29,10 @@ export default function App() {
     });
   };
 
-  // Обчислення загальної кількості голосів
+  
   const totalVotes = votes.good + votes.neutral + votes.bad;
 
-  // Обчислення відсотка позитивних голосів
+ 
   const positiveRate = totalVotes
     ? Math.round((votes.good / totalVotes) * 100)
     : 0;
